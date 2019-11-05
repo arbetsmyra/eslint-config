38e3aaad41ffed1ab4df2bef247d8052d1280f3e
Anthon Holmqvist <anthon.kendel@gmail.com>
2019-11-05 20:46:09 +0100

    Add basic rules

--- config/base.eslintrc.js
+++ config/base.eslintrc.js
@@ -1,5 +1,25 @@
+// Eslint rules: https://eslint.org/docs/rules/
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
   globals: {
-    Arbetsmyra: true,
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'linebreak-style': ['error', 'unix'],
+    'quote-props': ['error', 'as-needed'],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
   },
 };
