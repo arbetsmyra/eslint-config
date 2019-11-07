68b6d6d0cd49384d22609acd0b5fc48ad7b09ac2
Anthon Holmqvist <anthon.kendel@gmail.com>
2019-11-07 20:52:53 +0100

    Add best practices rules

--- config/base.eslintrc.js
+++ config/base.eslintrc.js
@@ -1,4 +1,5 @@
-// Eslint rules: https://eslint.org/docs/rules/
+// ESLint rules: https://eslint.org/docs/rules
+
 module.exports = {
   env: {
     browser: true,
@@ -15,6 +16,35 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    // Category: best practices
+    'array-callback-return': ['error'],
+    'block-scoped-var': ['error'],
+    complexity: ['error', 10],
+    curly: ['error'], // autofixable
+    'default-case': ['error'],
+    'default-param-last': ['error'],
+    'dot-notation': 'error', // autofixable
+    eqeqeq: ['error', 'always'], // autofixable
+    'max-classes-per-file': ['error', 1],
+    'no-else-return': ['error'], // autofixable
+    'no-empty-function': ['error'],
+    'no-eval': ['error'],
+    'no-floating-decimal': ['error'], // autofixable
+    'no-implicit-coercion': ['error'], // autofixable
+    'no-implied-eval': ['error'],
+    'no-lone-blocks': ['error'],
+    'no-multi-spaces': ['error'], // autofixable
+    'no-return-assign': ['error'],
+    'no-return-await': ['error'],
+    'no-script-url': ['error'],
+    'no-throw-literal': ['error'],
+    'no-unused-labels': ['error'], // autofixable
+    'no-useless-return': ['error'],
+    'prefer-regex-literals': ['error'],
+    radix: ['error', 'always'],
+    'wrap-iife': ['error', 'outside'], // autofixable
+    yoda: ['error'], // autofixable
+
     'comma-dangle': ['error', 'always-multiline'],
     'linebreak-style': ['error', 'unix'],
     'quote-props': ['error', 'as-needed'],
